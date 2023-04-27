a026d39994897647b85bc6115f0cf349dee5e9fa
RachidIRT <rachidsimor0941@gmail.com>
2023-04-27 16:32:20 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,47 +1,37 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+
 /**
- * add_node_end - Adds a new node at the end of a list
- *@head: pointer to the head of the list
- *@str: string to be added to the list
- *
- *Return: NULL in case of failure
- *or address or the element
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *number_of_nodes, *last;
-
-	number_of_nodes = malloc(sizeof(list_t));
+	list_t *number_of_nodes, *temp;
+	size_t nchar;
+		number_of_nodes = malloc(sizeof(list_t));
 	if (number_of_nodes == NULL)
 		return (NULL);
 
-	dup = strdup(str);
-	if (str == NULL)
+	number_of_nodes->str = strdup(str);
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+	number_of_nodes->len = nchar;
+	number_of_nodes->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
 	{
-		free(number_of_nodes);
-		return (NULL);
-	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	number_of_nodes->str = dup;
-	number_of_nodes->len = len;
-        number_of_nodes->next = NULL;
-
-	if (*head == NULL)
 		*head = number_of_nodes;
+	}
 	else
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = number_of_nodes;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = number_of_nodes;
 	}
 	return (*head);
 }
