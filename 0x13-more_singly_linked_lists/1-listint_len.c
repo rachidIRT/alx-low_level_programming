ac50fdbf90fd44939ff2255cfe3caa40f8909c2c
RachidIRT <rachidsimor0941@gmail.com>
2023-05-02 14:51:50 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,18 +2,18 @@
 /**
  * listint_len - the length of linked list
  * @h: the list
- * Return: the number of elements
+ * Return: the number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t name_radical = 0;
+	size_t name = 0;
 
 	if (h == NULL)
 		return (0);
 	if (h->next != NULL)
 	{
-		name_radical++;
+		name++;
 		h = h->next;
 	}
-	return (name_radical + 1);
+	return (name + 1);
 }
