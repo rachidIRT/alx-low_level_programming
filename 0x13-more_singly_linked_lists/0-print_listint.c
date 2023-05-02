2fd74dfd42dfb0e4f336c6b0b85686a73e7c4714
RachidIRT <rachidsimor0941@gmail.com>
2023-05-02 13:55:14 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
+
 /**
  * print_listint - prints all the elements of a linked list
  * @h: head of the list
  *
- * Return: the number 
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *cursor = h;
-	size_t lofi = 0;
+	size_t radical_number = 0;
 
 	while (cursor != NULL)
 	{
 		printf("%d\n", cursor->n);
-		lofi += 1;
+		radical_number += 1;
 		cursor = cursor->next;
 	}
-	return (lofi);
+	return (radical_number);
 }
