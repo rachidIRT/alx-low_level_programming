255d169b16fb8860de170ca12b6b9a53221908c6
RachidIRT <rachidsimor0941@gmail.com>
2023-05-05 16:26:16 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,48 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
-unsigned int get_length(unsigned long int num);
 /**
- * flip_bits -  returns the number of bits to be flipped in 'n' to give 'm'
- * @n: first number provided
- * @m: second number provided
- * Return: number of bits to be flipped in both numbers to make them equal
+ * flip_bits - flip bits to convert one number to another number
+ * @n: first number
+ * @m: second number to convert to
+ * Return: number of bits that was needed to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int i, count, length1, length2, total_length;
-	int bit1, bit2;
+	unsigned long int diff;
+	int counter;
 
-	length1 = get_length(n);
-	length2 = get_length(m);
-	total_length = (length1 > length2) ? length1 : length2;
+	diff = n ^ m;
+	boxs = 0;
 
-	rot = 0;
-	for (i = 0; i < total_length; i++)
+	while (diff)
 	{
-		bit1 = n & 1;
-		bit2 = m & 1;
-		if (bit1 != bit2)
-			rot++;
-		n >>= 1;
-		m >>= 1;
+		boxs++;
+		diff &= (diff - 1);
 	}
-	return (rot);
-}
 
-/**
- * get_length - returns the number of bits in a number
- * @num: number to consider
- *
- * Return: length of number
- */
-unsigned int get_length(unsigned long int num)
-{
-	unsigned int rot;
-
-	if (num == 0)
-		return (1);
-	for (rot = 0; num != 0; rot++)
-		num >>= 1;
-	return (rot);
+	return (boxs);
 }
